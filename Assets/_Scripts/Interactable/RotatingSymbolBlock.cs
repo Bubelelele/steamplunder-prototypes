5a40f470b7754863bb7868a899b4cfe949191738
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-03-08 13:03:10 +0100

    added puzzle

---
+++ Assets/_Scripts/Interactable/RotatingSymbolBlock.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RotatingSymbolBlock : MonoBehaviour, IInteractable {
+    
+    public int CurrentSymbol { get; private set; } = 1;
+    // 1-Clubs, 2-Diamonds, 3-Hearts, 4-Spades //
+    public event Action onRotation;
+    
+    [SerializeField] private float rotateDelay = .01f;
+    [SerializeField] private Transform blockTransform;
+
+    private bool _isRotating;
+
+    private void Start() {
+        //Random start symbol
+        CurrentSymbol = Random.Range(1, 5);
+        blockTransform.Rotate(new Vector3(0, (CurrentSymbol-1)*90f));
+    }
+
+    public void Interact() {
+        if (_isRotating) return;
+        
+        _isRotating = true;
+        StartCoroutine(Rotate());
+    }
+
+    public void StopInteract() { }
+
+    public string GetDescription() {
+        return _isRotating ? "(Rotating)" : "Rotate Block";
+    }
+
+    private IEnumerator Rotate() {
+        for (int i = 0; i < 90; i++) {
+            blockTransform.Rotate(new Vector3(0, 1));
+            yield return new WaitForSeconds(rotateDelay);
+        }
+
+        CurrentSymbol++;
+        if (CurrentSymbol > 4) CurrentSymbol = 1;
+        onRotation?.Invoke();
+        _isRotating = false;
+    }
+}
---
+++ Assets/_Scripts/Puzzle/RotatingSymbolBlockMaster.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RotatingSymbolBlockMaster : MonoBehaviour {
+
+    [SerializeField] private int[] solution;
+    [SerializeField] private RotatingSymbolBlock[] blocks;
+
+    private void Start() {
+        if (solution.Length != blocks.Length) Debug.LogWarning("Rotating block puzzle solution length error");
+
+        foreach (var block in blocks) {
+            block.onRotation += CheckSolution;
+        }
+    }
+
+    private void CheckSolution() {
+        for (int i = 0; i < blocks.Length; i++) {
+            if (blocks[i].CurrentSymbol != solution[i]) return;
+        }
+
+        Debug.Log("gj");
+    }
+}
