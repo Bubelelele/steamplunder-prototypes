619539e5896f7801a24ef8c77da5aa750565451e
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-03-11 14:54:42 +0100

    model updates

--- Assets/_Scripts/Effects/DamageFlash.cs
+++ Assets/_Scripts/Effects/DamageFlash.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 public class DamageFlash : MonoBehaviour {
 
     [SerializeField] private float flashTime = .15f;
-
-    private MeshRenderer _renderer;
+    [SerializeField] private MeshRenderer meshRenderer;
+    
     private Color _origColor;
 
     private void Start() {
-        _renderer = GetComponent<MeshRenderer>();
-        _origColor = _renderer.material.color;
+        _origColor = meshRenderer.material.color;
     }
 
     public void Flash() => StartCoroutine(FlashEffect());
 
     private IEnumerator FlashEffect() {
-        _renderer.material.color = Color.white;
+        Material[] materials = meshRenderer.materials;
+        foreach (Material material in materials) {
+            material.color = Color.white;
+        }
+
+        meshRenderer.materials = materials;
         yield return new WaitForSeconds(flashTime);
-        _renderer.material.color = _origColor;
+        foreach (Material material in materials) {
+            material.color = _origColor;
+        }
+
+        meshRenderer.materials = materials;
     }
 }
