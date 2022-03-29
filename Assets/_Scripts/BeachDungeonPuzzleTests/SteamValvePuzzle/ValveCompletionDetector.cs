abc89212ed18f6058d6016138b2918f7f8919cab
loldani123 <harestaddaniel@gmail.com>
2022-03-29 13:22:45 +0200

    Made Steam Pipe puzzle prot
    steam pipe puzzle prototype

---
+++ Assets/_Scripts/BeachDungeonPuzzleTests/SteamValvePuzzle/ValveCompletionDetector.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValveCompletionDetector : MonoBehaviour
+{
+    public ValveInteract[] valveList;
+
+    [SerializeField]private bool[] isComplete;
+
+    private void Awake()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        isComplete = new bool[valveList.Length];
+    }
+
+    private void Update()
+    {
+        // checks if the pipes are "on" or "off" and records it in isComplete
+        for (int i = 0; i < valveList.Length; i++)
+        {
+           isComplete[i] = !valveList[i].GetSteamActive();
+        }
+       
+        //changes the color of the square to green when it is complete and keeps it at red if it isn't
+       if (IsThePuzzleComplete())
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    private bool IsThePuzzleComplete()
+    {
+        for (int i = 0; i < isComplete.Length; i++)
+        {
+            if (isComplete[i] == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
