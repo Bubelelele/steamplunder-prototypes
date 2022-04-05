7422fa5474046ae654687c133af24fa98d26c882
istid1 <mhr97@hotmail.com>
2022-04-05 13:23:19 +0200

    CylinderPuzzleNotDone
    CylinderPuzzle without the real cylinder. And minor edits needed

---
+++ Assets/_Scripts/Other/CylinderPuzzle.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class CylinderPuzzle : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private bool clockwise = true;
+    [SerializeField] private float speed = 1f;
+    public GameObject pressurePlate;
+
+    private float _dir;
+
+    private void Awake()
+    {
+        _dir = clockwise ? -1 : 1;
+        
+    }
+
+	private void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up, rotationSpeed * _dir, Space.Self);
+        transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("MovingBox"))
+		{
+            speed = -speed;
+            _dir = -_dir;
+            
+        }
+           
+    }
+
+
+}
