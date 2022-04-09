2fbc015c58c56b650b89059c4e90810e1b279d0c
sort7er <98805454+sort7er@users.noreply.github.com>
2022-04-10 01:53:01 +0200

    Small fixes to chair

--- Assets/_Scripts/SteampunkColossus/ChairDestruction.cs
+++ Assets/_Scripts/SteampunkColossus/ChairDestruction.cs
@@ -10,17 +10,17 @@ public class ChairDestruction : MonoBehaviour
         if (other.gameObject.tag == "Boss" && !isHit)
         {
             isHit = true;
-            Debug.Log("Yup");
             chair.AddComponent<Rigidbody>();
             chair.GetComponent<Rigidbody>().isKinematic = false;
             chair.GetComponent<Rigidbody>().useGravity = true;
-            Vector3 recoilForceVector = (new Vector3(transform.position.x, transform.position.y + 3, transform.position.z) - other.transform.position).normalized * 15f;
+            chair.GetComponent<Rigidbody>().mass = 100;
+            Vector3 recoilForceVector = (new Vector3(transform.position.x, transform.position.y + 3, transform.position.z) - other.transform.position).normalized * 1200f;
             chair.GetComponent<Rigidbody>().AddForce(recoilForceVector, ForceMode.Impulse);
+            chair.GetComponent<Rigidbody>().AddTorque(recoilForceVector, ForceMode.Impulse);
         }
         else if (isHit && other.gameObject.tag != "Boss" && other.gameObject.tag != "Player")
         {
             Destroy(chair);
-            Debug.Log("Boom");
         }
     }
 }
