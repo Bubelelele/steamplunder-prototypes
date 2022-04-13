0666bb591644c513d5bb40ddfef6a11b2f71ade0
istid1 <mhr97@hotmail.com>
2022-04-13 07:25:24 +0200

    CylinderPuzzleEdit

--- Assets/_Scripts/Other/CylinderPuzzle.cs
+++ Assets/_Scripts/Other/CylinderPuzzle.cs
@@ -4,44 +4,41 @@ using System.Collections;
 public class CylinderPuzzle : MonoBehaviour
 {
 
-    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float rotationSpeed = 1.5f;
     [SerializeField] private bool clockwise = true;
     [SerializeField] private float speed = 0f;
     private float _dir;
+   
+
+    public GameObject Bean;
 
     private bool CanCollide = true;
 
     private void Awake()
     {
         _dir = clockwise ? -1 : 1;
-        speed = 2;
+        
     }
 
 	private void FixedUpdate()
     {
-		if (Input.GetButton("Fire2"))
-		{
-            
+  
             StartCoroutine(PuzzleStart());
-        }
-		if (Input.GetButtonUp("Fire2"))
-		{
-            speed = 2;
-            _dir = clockwise ? -1 : 1;
-        }
     }
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag("MovingBox") && CanCollide)
+		if (other.gameObject.CompareTag("MoveBox") && CanCollide)
 		{
-            StartCoroutine(Invinsible());
+           
+           
             speed = -speed;
             _dir = -_dir;
         }
 		if (other.gameObject.CompareTag("CorrectBox"))
 		{
-            Debug.Log("goodjob");
+            transform.Rotate(Vector3.up, rotationSpeed * _dir, Space.Self);
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
         }
 		if (other.gameObject.CompareTag("PuzzleSolved"))
 		{
@@ -53,15 +50,7 @@ public class CylinderPuzzle : MonoBehaviour
     IEnumerator PuzzleStart()
 	{
         transform.Rotate(Vector3.up, rotationSpeed * _dir, Space.Self);
-        transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
         yield return new WaitForSeconds(0);
     }
-    IEnumerator Invinsible()
-	{
-        CanCollide = false;
-        yield return new WaitForSeconds(0.5f);
-
-        CanCollide = true;
-        
-	}
 }
