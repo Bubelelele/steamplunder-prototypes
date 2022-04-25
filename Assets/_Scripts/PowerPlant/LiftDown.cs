af9e671b9ab06936339f3d84374c6ce173e8d5b8
istid1 <mhr97@hotmail.com>
2022-04-25 22:10:56 +0200

    PowerPlantEdits

--- Assets/_Scripts/PowerPlant/LiftDown.cs
+++ Assets/_Scripts/PowerPlant/LiftDown.cs
@@ -1,13 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class LiftDown : MonoBehaviour
 {
-    public Animator liftAnim;
+    public Animator[] liftAnim;
 
    public void LiftDownFunction()
     {
-        liftAnim.SetTrigger("LiftDown");
+        for (int i = 0; i < liftAnim.Length; i++)
+        {
+            liftAnim[i].SetTrigger("IsActive");
+        }
+        
     }
 }
