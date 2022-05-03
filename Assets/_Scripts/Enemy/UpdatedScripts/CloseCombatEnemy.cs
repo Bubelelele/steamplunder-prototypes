717e0c8c14e37e2a40637841d11d35685bf1a12e
sort7er <98805454+sort7er@users.noreply.github.com>
2022-05-03 19:17:47 +0200

    Small fixes

--- Assets/_Scripts/Enemy/UpdatedScripts/CloseCombatEnemy.cs
+++ Assets/_Scripts/Enemy/UpdatedScripts/CloseCombatEnemy.cs
@@ -6,24 +6,74 @@ public class CloseCombatEnemy : EnemyBase
     [HideInInspector] public bool lethal = false;
     [HideInInspector] public Vector3 targetPos;
 
-    public Transform swipeLeft, swipeRight;
+    //Sword
     public Renderer swordRenderer;
     public Material swordMat, swordMatLight;
 
+    //Movement
+    public Transform swipeLeft, swipeRight;
+
+
     private bool chasePlayer = false;
-    private bool pivotDirChoosen = false;
-    private int moveDir;
+    
+    //Waiting to attack
     private bool animationPlaying = false;
     private bool invokedOnce = false;
+
+    //Pivoting around the player
     private bool pivot = true;
+    private int moveDir;
+    private bool pivotDirChoosen = false;
+
+    //Stunning
     private bool isStunned = false;
     private bool canBeStunned = false;
-    private bool blockStun = false;
     private int side;
     private float distanceToPlayerBeforeStop = 3f;
 
 
+    protected override void UpdateSense()
+    {
+        if (canBeStunned)
+        {
 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (!isStunned)
+                {
+                    lethal = false;
+                    isStunned = true;
+                    enemyAnim.SetTrigger("Stunned");
+                    swordRenderer.materials[2] = swordMat;
+                    agent.SetDestination(transform.position);
+                }
+
+            }
+        }
+
+        if (chasePlayer && !animationPlaying)
+        {
+            if (Vector3.Distance(player.transform.position, transform.position) < distanceToPlayerBeforeStop)
+            {
+                agent.speed = movementSpeed;
+                agent.SetDestination(transform.position);
+                distanceToPlayerBeforeStop = 4f;
+                InAttackRange();
+            }
+            else if (Vector3.Distance(player.transform.position, transform.position) > distanceToPlayerBeforeStop)
+            {
+                agent.speed = movementSpeed;
+                agent.SetDestination(player.transform.position);
+                distanceToPlayerBeforeStop = 3f;
+                pivotDirChoosen = false;
+            }
+        }
+        else if (!chasePlayer)
+        {
+            agent.speed = movementSpeed / 2;
+            agent.SetDestination(homePoint);
+        }
+    }
 
     public override void EnemyInSight(){chasePlayer = true;}
     public override void EnemyOutOfSight(){chasePlayer = false;}
@@ -34,15 +84,15 @@ public class CloseCombatEnemy : EnemyBase
             agent.speed = movementSpeed / 2;
             if (!invokedOnce)
             {
-                Invoke("Attack", Random.Range(0.5f, 1.5f));
+                Invoke("Attack", Random.Range(0.5f, 2.5f));
                 invokedOnce = true;
             }
             else if (Input.GetMouseButtonDown(InputManager.instance.AxeMouseBtn))
             {
-                Invoke("Attack", 0.3f);
                 agent.speed = movementSpeed*5;
                 pivot = false;
                 CancelInvoke();
+                Invoke("Attack", 0.3f);
                 side = Random.Range(0, 2);
                 animationPlaying = true;
                 if (side == 0) { agent.SetDestination(swipeLeft.position);}
@@ -74,63 +124,11 @@ public class CloseCombatEnemy : EnemyBase
             enemyAnim.SetInteger("Swing", Random.Range(1, 4));
             agent.SetDestination(transform.position);
     }
-    protected override void UpdateSense()
-    {
-        //Blocking
-        if (canBeStunned)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (!isStunned)
-                {
-                    lethal = false;
-                    isStunned = true;
-                    enemyAnim.SetTrigger("Stunned");
-                    ChangeSwordMat(swordMat);
-                    agent.SetDestination(transform.position);
-                }
-
-            }
-        }
-        else if (!canBeStunned)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                blockStun = true;
-            }
-        }
-
-        if (chasePlayer && !animationPlaying)
-        {
-            if (Vector3.Distance(player.transform.position, transform.position) < distanceToPlayerBeforeStop)
-            {
-                agent.speed = movementSpeed;
-                agent.SetDestination(transform.position);
-                distanceToPlayerBeforeStop = 4f;
-                InAttackRange();
-            }
-            else if(Vector3.Distance(player.transform.position, transform.position) > distanceToPlayerBeforeStop)
-            {
-                agent.speed = movementSpeed;
-                agent.SetDestination(player.transform.position);
-                distanceToPlayerBeforeStop = 3f;
-                pivotDirChoosen = false;
-            }
-        }
-        else if (!chasePlayer)
-        {
-            agent.speed = movementSpeed /2;
-            agent.SetDestination(homePoint);
-        }
-    }
+    
     public void CanBeStunned()
     {
-        if (!blockStun)
-        {
-            canBeStunned = true;
-            ChangeSwordMat(swordMatLight);
-        }
-        
+        canBeStunned = true;
+        ChangeSwordMat(swordMatLight);
     }
     public void CannnotBeStunned()
     {
@@ -152,7 +150,6 @@ public class CloseCombatEnemy : EnemyBase
     }
     public void AnimationDone()
     {
-        blockStun = false;
         animationPlaying = false;
         invokedOnce = false;
         pivot = true;
