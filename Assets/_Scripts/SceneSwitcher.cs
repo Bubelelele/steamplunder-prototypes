fe2080b2416adf16550d3373c9cc444a873ff818
sort7er <98805454+sort7er@users.noreply.github.com>
2022-05-04 13:38:25 +0200

    Showcase

--- Assets/_Scripts/SceneSwitcher.cs
+++ Assets/_Scripts/SceneSwitcher.cs
@@ -10,7 +10,5 @@ public class SceneSwitcher : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha3)) SceneManager.LoadScene(3);
         if (Input.GetKeyDown(KeyCode.Alpha4)) SceneManager.LoadScene(4);
         if (Input.GetKeyDown(KeyCode.Alpha5)) SceneManager.LoadScene(5);
-        if (Input.GetKeyDown(KeyCode.Alpha6)) SceneManager.LoadScene(6);
-        if (Input.GetKeyDown(KeyCode.Alpha7)) SceneManager.LoadScene(7);
     }
 }
